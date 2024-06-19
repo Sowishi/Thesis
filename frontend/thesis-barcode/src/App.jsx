985949aee392a9c3585e0fff09cc13912f9c8a0f
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-06-19 22:45:10 +0800

    created the sidebar

--- frontend/thesis-barcode/src/App.jsx
+++ frontend/thesis-barcode/src/App.jsx
@@ -1,11 +1,12 @@
 import { Button } from "flowbite-react";
 import Login from "./pages/auth/login";
 import Forgot from "./pages/auth/forgot";
+import { Scan } from "./pages/admin/scan";
 
 const App = () => {
   return (
     <>
-      <Login />
+      <Scan />
     </>
   );
 };
