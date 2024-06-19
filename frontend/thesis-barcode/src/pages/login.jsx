b8b562d26726fda1525323557621ff01cfb6ab71
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-06-19 22:16:50 +0800

    Add background text in login page

--- frontend/thesis-barcode/src/pages/login.jsx
+++ frontend/thesis-barcode/src/pages/login.jsx
@@ -2,8 +2,21 @@ import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
 const Login = () => {
   return (
-    <div className="w-full flex justify-center items-center h-screen  bg-slate-900">
-      <form className="flex flex-col gap-4 w-96 p-10 bg-slate-950 rounded-lg text-white">
+    <div className="w-full flex justify-center items-center h-screen  bg-slate-900 relative">
+      <h1
+        className="text-white text-center hidden md:flex"
+        style={{
+          fontSize: "13rem",
+          position: "absolute",
+          top: 0,
+          opacity: 0.2,
+          zIndex: 0,
+        }}
+      >
+        Smart Library
+      </h1>
+
+      <form className="flex flex-col gap-4 w-96 p-10 bg-slate-950 rounded-lg text-white z-50">
         <div>
           <div className="mb-2 block">
             <Label className="text-white" htmlFor="email1" value="Your email" />
